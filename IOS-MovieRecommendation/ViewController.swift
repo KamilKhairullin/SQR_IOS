1d2f830c841bdd194a3b8ec0ccb6319a0420920e
Kamil Khairullin <kamil.khayrullin.personal@yandex.ru>
2022-03-11 22:39:07 +0300

    blank project

---
+++ IOS-MovieRecommendation/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  IOS-MovieRecommendation
+//
+//  Created by Kamil on 11.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
