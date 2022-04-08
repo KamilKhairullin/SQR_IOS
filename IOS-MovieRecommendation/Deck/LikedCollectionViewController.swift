9576d1583dd0af017f2ee6ac56a4f15dc6276dbc
Beka_l3 <b.talgat@innopolis.university>
2022-04-08 22:55:41 +0300

    Initial setup

---
+++ IOS-MovieRecommendation/Deck/LikedCollectionViewController.swift
@@ -0,0 +1,78 @@
+//
+//  LikedCollectionViewController.swift
+//  IOS-MovieRecommendation
+//
+//  Created by Bekzhan Talgat on 08.04.2022.
+//
+
+import Foundation
+import UIKit
+
+
+
+protocol LikedCollectionDelegate: AnyObject {
+    func addMovieToCollection(movie: Movie)
+    func getLikedMovieAmount() -> Int
+}
+
+
+class LikedCollectionViewController: UIViewController {
+    
+    private var movieCollection: [Movie]
+    
+    
+    
+    
+    
+// MARK: -- override functions
+    init(){
+        
+        self.movieCollection = [Movie]()
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .black
+        
+        setupViews()
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        
+    }
+    
+    
+    
+// MARK: -- functions
+    
+    private func setupViews() {
+        
+    }
+    
+    
+    
+}
+
+
+
+extension LikedCollectionViewController: LikedCollectionDelegate {
+    func addMovieToCollection(movie: Movie) {
+        movieCollection.append(movie)
+    }
+    
+    func getLikedMovieAmount() -> Int {
+        return movieCollection.count
+    }
+    
+    
+}
